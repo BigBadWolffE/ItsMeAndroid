b0b91d05901bd878c74e28ce4df47d1d8b0814be
Zharfan <after.dear@gmail.com>
2019-12-16 17:21:51 +0700

    share image ktp, take photo profile and ktp updated

--- app/src/main/java/com/indocyber/itsmeandroid/view/profile/activity/ProfileActivity.java
+++ app/src/main/java/com/indocyber/itsmeandroid/view/profile/activity/ProfileActivity.java
@@ -1,9 +1,25 @@
 package com.indocyber.itsmeandroid.view.profile.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.indocyber.itsmeandroid.R;
@@ -13,11 +29,20 @@ import com.indocyber.itsmeandroid.view.profile.fragment.ProfileKTPFragment;
 import com.indocyber.itsmeandroid.view.profile.fragment.ProfileNPWPFragment;
 import com.indocyber.itsmeandroid.view.profile.fragment.ProfilePassportFragment;
 
+import java.io.File;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileActivity extends AppCompatActivity {
 
+    protected static final int CAMERA_REQUEST = 0;
+    protected static final int GALLERY_PICTURE = 1;
     private TabAdapter mTabAdapter;
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private ImageView mTakePhotoBtn;
+    private CircleImageView mFotoProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +57,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         mViewPager = findViewById(R.id.viewPagerProfile);
         mTabLayout = findViewById(R.id.tabLayoutProfile);
+        mFotoProfile = findViewById(R.id.imageFotoProfile);
+        mTakePhotoBtn = findViewById(R.id.imageTakeFotoProfile);
+
         mTabAdapter = new TabAdapter(getSupportFragmentManager());
         mTabAdapter.addFragment(new DetailProfileFragment(), "Detail Profile");
         mTabAdapter.addFragment(new ProfileKTPFragment(), "KTP");
@@ -39,5 +67,74 @@ public class ProfileActivity extends AppCompatActivity {
         mTabAdapter.addFragment(new ProfilePassportFragment(), "Passport");
         mViewPager.setAdapter(mTabAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
+
+        mTakePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTakePhotoDialog();
+            }
+        });
+    }
+
+    private void startTakePhotoDialog() {
+        final AlertDialog.Builder myAlertDialog =
+                new AlertDialog.Builder(this, R.style.Theme_MaterialComponents_Light_Dialog_Alert);
+        myAlertDialog.setTitle("Select Option");
+        myAlertDialog.setMessage("How do you want to set your picture?");
+
+        myAlertDialog.setPositiveButton("Gallery",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        pickFromGallery();
+                    }
+                });
+
+        myAlertDialog.setNegativeButton("Camera",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        takeFromCamera();
+                    }
+                });
+
+        myAlertDialog.show();
+    }
+
+    private void pickFromGallery() {
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        startActivityForResult(intent,GALLERY_PICTURE);
+    }
+
+    private void takeFromCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            switch (requestCode) {
+                case CAMERA_REQUEST:
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    mFotoProfile.setImageBitmap(imageBitmap);
+                    break;
+                case GALLERY_PICTURE:
+                    Uri uri = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                        // Log.d(TAG, String.valueOf(bitmap));
+                        mFotoProfile.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
     }
 }
