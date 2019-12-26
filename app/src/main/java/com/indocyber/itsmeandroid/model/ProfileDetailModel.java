023c70602905964a16aed7eeab9a5619e36058c1
Zharfan <after.dear@gmail.com>
2019-12-26 11:54:35 +0700

    detail profile dummy save

---
+++ app/src/main/java/com/indocyber/itsmeandroid/model/ProfileDetailModel.java
@@ -0,0 +1,68 @@
+package com.indocyber.itsmeandroid.model;
+
+public class ProfileDetailModel {
+
+    private String namaLengkap;
+    private String email;
+    private String noTelp;
+    private String password;
+    private String pin;
+    private String alamat;
+    private String hobby;
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
