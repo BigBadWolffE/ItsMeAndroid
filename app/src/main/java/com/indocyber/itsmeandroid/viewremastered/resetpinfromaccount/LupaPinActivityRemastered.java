e3524a6b132b49747fb1067e2d977903e337adfe
Big Bad Wolf <wisnuluck96@gmail.com>
2020-03-23 09:15:17 +0700

    push

---
+++ app/src/main/java/com/indocyber/itsmeandroid/viewremastered/resetpinfromaccount/LupaPinActivityRemastered.java
@@ -0,0 +1,16 @@
+package com.indocyber.itsmeandroid.viewremastered.resetpinfromaccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.indocyber.itsmeandroid.R;
+
+public class LupaPinActivityRemastered extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupa_pin_remastered);
+    }
+}
